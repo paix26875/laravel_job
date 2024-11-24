dfbceac6c4ceddcc83873dba047e7dd82a3c7e6b
paix26875 <shimada2022@gmail.com>
2024-11-24 17:12:04 +0900

    feat: ちょっと見た目を調整

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,8 +12,12 @@
 
         <script src="https://cdn.tailwindcss.com"></script>
         <script>
-            const fetchProgress = () => {
-                fetch('/api/progress')
+            const sleep = (time) => new Promise((resolve) => setTimeout(resolve, time));
+            const fetchProgress = async () => {
+                document.getElementById('jobProgress').innerText = '更新中…';
+                document.getElementById('jobProgressPercent').innerText = '';
+                await sleep(400);
+                await fetch('/api/progress')
                     .then(response => response.json())
                     .then(data => {
                         console.log(data);
@@ -25,6 +29,7 @@
                         document.getElementById('jobProgressPercent').innerText = (data.totalJobCount - (data.waitingJobCount + data.wipJobCount)) / data.totalJobCount * 100 + ' %';
                     }).catch(error => {
                         console.error('Error:', error);
+                        document.getElementById('jobProgress').innerText = 'エラー';
                 });
             }
             window.onload = function () {
@@ -36,20 +41,20 @@
     <body>
         <main class="m-6">
             <div>
-                <a href="{{route('horizon.index')}}">
+                <a href="{{route('horizon.index')}}" class="text-blue-500 underline-offset-auto	underline">
                     Horizonのページへ
                 </a>
                 <div class="flex flex-col gap-6">
                     <div>
-                        <h2>Jobの操作</h2>
+                        <h2 class="text-xl">Jobの操作</h2>
                         <div class="flex flex-col gap-2">
-                            <a href="{{route('enqueue')}}" class="px-3 py-2 dark:bg-blue-500 text-white rounded-full text-center">キューイング</a>
-                            <a href="{{route('reset')}}" class="px-3 py-2 dark:bg-blue-500 text-white rounded-full text-center">リセット</a>
+                            <a href="{{route('enqueue')}}" class="px-3 py-2 bg-blue-500 text-white rounded-full text-center w-96">キューイング</a>
+                            <a href="{{route('reset')}}" class="px-3 py-2 bg-blue-500 text-white rounded-full text-center w-96">リセット</a>
                         </div>
                     </div>
                     <div class="flex flex-col gap-2">
-                        <h2>Jobの進行状況</h2>
-                        <button  class="px-3 py-2 dark:bg-blue-500 text-white rounded-full text-center" id="get-progress">更新</button>
+                        <h2 class="text-xl">Jobの進行状況</h2>
+                        <button  class="px-3 py-2 bg-blue-500 text-white rounded-full text-center w-96" id="get-progress">更新</button>
 
                         <div class="flex flex-col gap-2">
                             <p id="jobProgress"></p>
